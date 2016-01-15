f4d59046d5b57044d17e992faf083601a6d03625
Max Rayman <maxrayman@Maxs-MacBook-Pro.local>
2016-01-14 20:16:57 -0500

    Chapter 3 Completed

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -22,17 +22,19 @@ function randomPlay() {
 ////////////////////////////////////////////////
 
 function getPlayerMove(move) {
-    // Write an expression that operates on a variable called `move`
-    // If a `move` has a value, your expression should evaluate to that value.
-    // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* Your Expression */;
+    if (move == true){
+        return move;
+    } else { 
+        getInput();
+    }
 }
 
 function getComputerMove(move) {
-    // Write an expression that operates on a variable called `move`
-    // If a `move` has a value, your expression should evaluate to that value.
-    // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return /* Your Expression */;
+    if (move == true){
+        return move;
+    } else { 
+        randomPlay();
+    }
 }
 
 function getWinner(playerMove,computerMove) {
